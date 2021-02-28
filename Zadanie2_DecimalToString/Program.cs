216624d72dc9273420fc74e0be2fa55966c55ed7
Siliava <47508228+Siliava@users.noreply.github.com>
2021-02-28 19:12:32 +0300

    Добавьте файлы проекта.

---
+++ Zadanie2_DecimalToString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadanie2_DecimalToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Decimal");
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            int remainder;
+            string result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;//остаток
+                decimalNumber /= 2;//делитель
+                result = remainder.ToString()+ result;
+            }
+
+            Console.WriteLine("Binary: {0}", result);
+            Console.ReadKey();
+            
+        }
+    }
+
+}
